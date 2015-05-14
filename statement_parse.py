754d1a2130b40036be6d7625006a981b3fdc8a28
carlospiad <carlospiad@gmail.com>
2015-05-14 18:05:20 -0500

    cleanup and add gitignore

--- statement_parse.py
+++ statement_parse.py
@@ -9,21 +9,3 @@ with open('data/000101027265_0415_20150514164017.csv', newline='') as csvfile:
     for line in cs:
         print (line)
         writer.writerow((line[4],line[8],line[7],'',line[5],line[6]))
-
-
-# with open('MovimientoEst.txt', newline='',encoding='latin-1' ) as f:
-#     reader = csv.reader(f,delimiter=';')
-#     fecha = ''
-#     for row in reader:
-#         fecha= fecha + row[0]+","
-#         # print(row[1])
-#         # print(row[2])
-#         # print(row[3])
-#         # print(row[4])
-#         # print(row[5])  balance
-#
-# print (type (fecha))
-#
-# with open("output.csv", "wb") as f:
-#     writer = csv.writer(f)
-#     writer.writerows(fecha)
